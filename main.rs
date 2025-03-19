04cf9a32c13328c4757dc0470dc7bd8899bcbd56
NPV16 <trans116@gmail.com>
2025-03-19 18:23:49 +0300

    Add files via upload

--- main.rs
+++ main.rs
@@ -382,13 +382,13 @@ async fn generate_links() {
     let mut list_file = File::create(format!("{}/list.txt", file_path))
         .expect("Failed to create list.txt");
 
-    writeln!(list_file, "vless://{}@{}:{}?encryption=none&security=tls&sni={}&type=ws&host={}&path=%2Fvless-argo%3Fed%3D2048#{}-{}",
+    writeln!(list_file, "vless://{}@{}:{}?encryption=none&security=tls&sni={}&type=ws&host={}&path=/#{}-{}",
         uuid, cfip, cfport, argodomain, argodomain, name, isp).unwrap();
     
     writeln!(list_file, "\nvmess://{}", 
         BASE64_STANDARD.encode(serde_json::to_string(&vmess_config).unwrap())).unwrap();
     
-    writeln!(list_file, "\ntrojan://{}@{}:{}?security=tls&sni={}&type=ws&host={}&path=%2Ftrojan-argo%3Fed%3D2048#{}-{}",
+    writeln!(list_file, "\ntrojan://{}@{}:{}?security=tls&sni={}&type=ws&host={}&path=/#{}-{}",
         uuid, cfip, cfport, argodomain, argodomain, name, isp).unwrap();
 
     let list_content = fs::read_to_string(format!("{}/list.txt", file_path))
